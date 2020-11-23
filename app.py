e48f392fba80a31db6e3a2e1c0b6c0e05d706d7f
bumbarad <47971678+bumbarad@users.noreply.github.com>
2020-11-23 10:33:35 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import flask
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, make_response, jsonify, redirect, url_for, request
 from flask_restplus import Api, Resource
 from twitch import Twitch
 import requests
@@ -10,92 +10,60 @@ api_bp = flask.Blueprint("api", __name__, url_prefix="/api")
 api = Api(api_bp, title="Esports-info API", version="1.0", doc="/docs")
 app.register_blueprint(api_bp)
 
-
-twitch_id = ""
-twitch_token = ""
-channels = ["esl_csgo", "dreamhackcs"]
+twitch: Twitch
 
 
-@api.route('/info/<string:name>', endpoint='info')
+@api.route('/info/<string:name>', endpoint='info', methods=['GET'])
 @api.doc(params={'name': 'Name of target twitch account.'})
 class InfoAPI(Resource):
     def get(self, name):
-        return {"data": name}
-
-
-# @api.route('/custom/<string:name>', endpoint='info')
-# @api.doc(params={'name': 'Name of target twitch account.'})
-# class CreateCustomPageAPI(Resource):
-#     def post(self, name):
-#         return {"data": name}
-
-
-def authorized_twitch_query(url: str, header=None):
-    headers = {'Authorization': 'Bearer ' + twitch_token, 'Client-Id': twitch_id}
-    if header is not None:
-        headers.update(header)
-    return requests.get(url, headers=headers).json()
-
-
-def get_info_about_twitch_stream(name: str):
-    return authorized_twitch_query("https://api.twitch.tv/helix/search/channels?query=" + name)
-
-
-def is_online(info: dict) -> bool:
-    return bool(info['data'][0]['is_live'])
-
-
-def parse_team_names(title: str) -> list:
-    teams = []
-    ret = []
-    for team in teams:
-        if title.find(team):
-            ret.append(team)
-    return ret
-
-
-def my_renderer(html: str, this: int):
-    twitch_data = []
-    online_streams = []
-    teams = []
-    for i in range(len(channels)):
-        twitch_data.append(get_info_about_twitch_stream(channels[i]))
-        online_streams.append(is_online(twitch_data[i]))
-
-    if this != -1:
-        if online_streams[this]:
-            stream = "https://player.twitch.tv/?channel=" + channels[this] + "&parent=127.0.0.1"
-            # teams = parse_team_names(twitch_data[this]['data'][0]['title'])
+        info = twitch.get_info_about(name)['data']
+        if len(info) > 0:
+            return info[0]
         else:
-            videos = authorized_twitch_query("https://api.twitch.tv/helix/videos?user_id="
-                                             + twitch_data[this]['data'][0]['id'], header={'type': 'archive'})
-            stream = "https://player.twitch.tv/?video=" + videos['data'][0]['id'] + "&parent=127.0.0.1"
-        return render_template(html, online_streams=online_streams, is_online=online_streams[this], stream=stream)
-    else:
-        return render_template(html, online_streams=online_streams, stream=False)
+            return make_response(jsonify("Name " + name + " not found"), 404)
+
+
+@api.route('/custom', endpoint='custom', methods=['GET', 'POST', 'DELETE'])
+class CreateCustomPageAPI(Resource):
+    def get(self):
+        return redirect(url_for('index'))
+
+    def post(self):
+        info = twitch.get_info_about(request.form['channel'])['data']
+        if len(info) > 0:
+            twitch.add_stream(info[0]['display_name'])
+            return redirect(url_for('custom', name=info[0]['display_name']))
+        else:
+            return make_response(jsonify("Name " + request.form['channel'] + " not found"), 404)
+
+    def delete(self):
+        if request.form['channel'] in twitch.streams:
+            twitch.streams.pop(request.form['channel'])
+        return redirect(url_for('index'))
 
 
 @app.route('/')
 def index():
-    return my_renderer("index.html", -1)
+    twitch.refresh_streams()
+    return render_template("index.html", streams=twitch.streams, stream_url=False, stream=False)
 
 
-@app.route('/esl')
-def esl():
-    return my_renderer("esl.html", 0)
-
-
-@app.route('/dreamhack')
-def dreamhack():
-    return my_renderer("dreamhack.html", 1)
+@app.route('/custom/<string:name>')
+def custom(name: str):
+    twitch.refresh_streams()
+    return render_template("custom.html", streams=twitch.streams, stream_url=twitch.get_stream_url(name), stream=name)
 
 
 if __name__ == "__main__":
-    twitch_credentials = open(str(Path(__file__).parent) + "\\static\\twitch_api.txt")
-    twitch_id = twitch_credentials.readline().strip()
-    twitch_secret = twitch_credentials.readline().strip()
+    twitch = Twitch()
+    twitch.add_stream("esl_csgo")
+    twitch.streams['esl_csgo']['static'] = True
+    twitch.streams['esl_csgo']['display_name'] = "ESL"
+    twitch.streams['esl_csgo']['banner'] = '/static/images/esl_logo.png'
 
-    twitch_token_url = "https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&grant_type=client_credentials"
-    twitch_token = requests.post(twitch_token_url.format(twitch_id, twitch_secret)).json()['access_token']
-
-    app.run(debug=True, )
+    twitch.add_stream("dreamleague")
+    twitch.streams['dreamleague']['static'] = True
+    twitch.streams['dreamleague']['display_name'] = "Dreamhack"
+    twitch.streams['dreamleague']['banner'] = '/static/images/dreamhack_logo.png'
+    app.run(debug=True)
--- twitch.py
+++ twitch.py
@@ -11,36 +11,57 @@ class Twitch:
         if credential_path is None:
             credential_path = str(Path(__file__).parent) + "\\static\\twitch_api.txt"
 
-        self.credentials = open(credential_path)
-        self.id = self.credentials.readline().strip()
-        self.secret = self.credentials.readline().strip()
-        self.token = requests.post("https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&grant_type=client_credentials".format(self.id, self.secret)).json()['access_token']
-        self.streams = []
+        self.__credentials = open(credential_path)
+        self.__id = self.__credentials.readline().strip()
+        self.__secret = self.__credentials.readline().strip()
+        self.__token = requests.post("https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&grant_type=client_credentials".format(self.__id, self.__secret)).json()['access_token']
+        self.streams = {}
 
-    def query(self, url: str, header=None):
-        headers = {'Authorization': 'Bearer ' + self.token, 'Client-Id': self.id}
+    def __query(self, url: str, header=None):
+        headers = {'Authorization': 'Bearer ' + self.__token, 'Client-Id': self.__id}
         if header is not None:
             headers.update(header)
         return requests.get(url, headers=headers).json()
 
     def get_info_about(self, channel_name: str):
-        return self.query("https://api.twitch.tv/helix/search/channels?query=" + channel_name)
+        return self.__query("https://api.twitch.tv/helix/search/channels?query=" + channel_name)
 
-    def get_offline_stream_url(self, channel_id: str) -> str:
-        videos = self.query("https://api.twitch.tv/helix/videos?user_id=" + channel_id, header={'type': 'archive'})
+    def __get_offline_stream_url(self, channel_id: str) -> str:
+        videos = self.__query("https://api.twitch.tv/helix/videos?user_id=" + channel_id, header={'type': 'archive'})
         return "https://player.twitch.tv/?video=" + videos['data'][0]['id'] + "&parent=127.0.0.1"
 
     def add_stream(self, channel_name: str):
-        self.streams.append(self.get_info_about(channel_name))
+        info = self.get_info_about(channel_name)
+        if len(info['data']) > 0:
+            self.streams[info['data'][0]['display_name']] = info['data'][0]
+            self.streams[info['data'][0]['display_name']]['static'] = False
+
+    def get_stream_url(self, channel_name: str) -> str:
+        if self.is_online(self.streams[channel_name]):
+            return self.__get_online_stream_url(channel_name)
+        return self.__get_offline_stream_url(self.get_channel_id(self.streams[channel_name]))
 
     @staticmethod
     def get_channel_id(info: dict) -> str:
-        return info['data'][0]['id']
+        return info['id']
 
     @staticmethod
-    def get_online_stream_url(channel_name: str) -> str:
+    def __get_online_stream_url(channel_name: str) -> str:
         return "https://player.twitch.tv/?channel=" + channel_name + "&parent=127.0.0.1"
 
     @staticmethod
     def is_online(info: dict) -> bool:
-        return bool(info['data'][0]['is_live'])
+        return bool(info['is_live'])
+
+    def refresh_streams(self):
+        for key in self.streams.keys():
+            static = self.streams[key]['static']
+            nm = self.streams[key]['display_name']
+            banner = None
+            if 'banner' in self.streams[key]:
+                banner = self.streams[key]['banner']
+            self.add_stream(key)
+            self.streams[key]['static'] = static
+            self.streams[key]['display_name'] = nm
+            if banner is not None:
+                self.streams[key]['banner'] = banner
